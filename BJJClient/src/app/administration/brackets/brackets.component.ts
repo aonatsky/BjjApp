c154711891540e4909937784ca0d1fb1fc0ea51f
Andrey Onatsky <a.onatsky@pnn.in.ua>
2017-01-25 18:42:50 +0200

    bracket WIP

--- BJJClient/src/app/administration/brackets/brackets.component.ts
+++ BJJClient/src/app/administration/brackets/brackets.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild,ViewEncapsulation } from '@angular/core';
 import { DataService } from '../../core/dal/contracts/data.service'
 import { Fight } from '../../core/model/fight.model'
 import { Fighter } from '../../core/model/fighter.model'
@@ -9,7 +9,8 @@ import { Fighter } from '../../core/model/fighter.model'
 @Component({
     selector: 'brackets',
     templateUrl: './brackets.component.html',
-    styleUrls: ['./brackets.component.css']
+    styleUrls: ['./brackets.component.css'],
+    encapsulation: ViewEncapsulation.None
 
 })
 
@@ -21,6 +22,7 @@ export class BracketsComponent implements OnInit {
     columnsCount: number[];
     fighters: Fighter[]
 
+    tableArray: string[][];
 
     fights: Fight[];
     private dataService: DataService;
@@ -36,6 +38,7 @@ export class BracketsComponent implements OnInit {
         this.dataService.getFigters(null).subscribe(data => this.fighters = data);
         this.rowsCount = this.getNumbersArray(this.getRowsCount());
         this.columnsCount = this.getNumbersArray(this.getColumnsCount());
+        this.tableArray = this.populateTable(this.getTableArray(11, 7))
 
     }
 
@@ -54,8 +57,67 @@ export class BracketsComponent implements OnInit {
         return Array(count).fill(0).map((x, i) => i); // [0,1,2,3,4]
     }
 
+    getTableArray(width: number, height: number): string[][] {
+        let tableArray = [];
+        for (var i = 0; i < width; i++) {
+            tableArray[i] = [];
+            for (var j = 0; j < height; j++) {
+                tableArray[i][j] = "";
+            }
+        }
+        return tableArray;
+
+    }
+
+    populateTable(table: string[][]): string[][] {
+        let result = table;
+        let width = 11;
+        let height = 7;
+        let halfWidth = 11 - 1;
+        for (var x = 0; x < width; x = x + 2) {
+            let fightsCount = x == 0? 4 : 4/x;
+            //result[x] = this.getRound(x,fightsCount,result[x])
+            let ystart = x - 1 >= 0 ? x - 1 : 0;
+            let yend = height - x;
+            for (var y = ystart; y < yend; y = y + 2) {
+                if (y%2 == 0) {
+                    result[x][y] = `<div>FIGHTER` + x.toString() + y.toString() + `</div>`    
+                }else{
+                    result[x][yend - y] = `<div>FIGHTER` + x.toString() + y.toString() + `</div>`    
+                }
+                
+            }
+
+        }
+
+        return result;
+
+    }
+
+    getRound(x: number, fightsCount: number, emptyRound: string[]) {
+        var result = emptyRound;
+        for (var i = 0; i < fightsCount; i++) {
+            let y = 0;
+            if (i % 2 == 0) {
+                y = result.length - 1 - x * i;
+            } else {
+                y = x * i;
+            }
+            result[y] = `<div>FIGHTER` + x.toString() + y.toString() + `</div>`
+
+        }
+        return result;
 
 
+    }
+
+    
+    divFighter = `<div class="input-group">
+      <div class="form-control">Team1</div><span class="input-group-addon"><span class="badge pull-right"></span></span>
+    </div>`;
+
+      divleftBottomCorner = 
+      `<table class='bracket-separator'><tr><td></td><td></td></td></tr><tr><td></td><td></td></td></tr></table>`;
 
 }
 
