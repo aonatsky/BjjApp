c0e110f4ced2a83fd240c4fae85ab00cee0d7684
Andrey Onatsky <a.onatsky@pnn.in.ua>
2018-03-20 18:02:54 +0200

    Results component created

---
+++ TRNMNT/ClientApp/app/event-admin/event-management/results/results.component.ts
@@ -0,0 +1,31 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+import { Input } from '@angular/core';
+import {CategorySimpleModel} from '../../../core/model/category.models';
+import {CategoryService} from '../../../core/services/category.service';
+
+@Component({
+    selector: 'results',
+    templateUrl: 'results.component.html',
+    encapsulation: ViewEncapsulation.None
+})
+
+export class ResultsComponent {
+    @Input() eventId: string;
+    private categories: CategorySimpleModel[] = [];
+    private selectedCategories: CategorySimpleModel[] = [];
+
+    constructor(private categoryService: CategoryService) {
+
+    }
+
+    ngOnInit() {
+        this.categoryService.getCategoriesByEventId(this.eventId).subscribe(r => {
+            this.categories = r;
+        });
+
+    }
+
+
+}
+
