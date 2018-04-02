df9b3110abcacd66d62e79d32e34e08e3021890f
Andrey Onatsky <a.onatsky@pnn.in.ua>
2018-04-02 15:06:56 +0300

    Migration Added

---
+++ TRNMNT.Data/Migrations/20180402120637_RoundUpdate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TRNMNT.Data.Migrations
+{
+    public partial class RoundUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "RoundResultDetails",
+                table: "Round",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoundResultType",
+                table: "Round",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoundResultDetails",
+                table: "Round");
+
+            migrationBuilder.DropColumn(
+                name: "RoundResultType",
+                table: "Round");
+        }
+    }
+}
