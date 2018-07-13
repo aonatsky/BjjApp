8679b68f7dfe90888cbb9b2056b834e7a1ebb8a1
Andrey Onatsky <a.onatsky@pnn.in.ua>
2018-07-13 14:31:34 +0300

    init

--- ci/InitEnvironment.ps1
+++ ci/InitEnvironment.ps1
@@ -1,4 +1,5 @@
-$env:PGPASSWORD = 'ZRTeam2017';
+$pwd = Read-Host -Prompt 'Input your pgsql password for user POSTGRES'
+$env:PGPASSWORD = $pwd;
 psql --username=postgres -w -c "CREATE DATABASE trnmnt_app_dev"
 psql --username=postgres -w -c "CREATE USER TRNMNTAPPDEV with PASSWORD '1'"
 psql --username=postgres -w -c "GRANT ALL ON DATABASE trnmnt_app_dev TO TRNMNTAPPDEV;"
